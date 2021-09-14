e9608ad3813cd8881709c8d95e628bedfb10adcc
Khang Nguyen <77101686+Mcbeany@users.noreply.github.com>
2021-09-14 12:26:51 +0700

    prepare

---
+++ src/BetterMinion.php
@@ -0,0 +1,13 @@
+<?php
+declare(strict_types=1);
+
+namespace Mcbeany\BetterMinion;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\SingletonTrait;
+
+class BetterMinion extends PluginBase
+{
+    use SingletonTrait;
+    //TODO
+}
--- src/Mcbeany/BetterMinion/utils/MinionLimiter.php
+++
@@ -1,9 +0,0 @@
-<?php
-declare(strict_types=1);
-
-namespace Mcbeany\BetterMinion\utils;
-
-class MinionLimiter
-{
-
-}
